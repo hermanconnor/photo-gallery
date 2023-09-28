d46c822b5d7a51b849b8cf92ad054518c0d94426
herman <hermanconnor11@gmail.com>
2023-09-28 15:28:23 -0400

    Create theme navbar, search, and theme switcher

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,31 @@
+import Link from 'next/link';
+import { Lobster } from 'next/font/google';
+
+import Search from '@/components/Search';
+import ThemeSwitcher from '@/components/ThemeSwitcher';
+import { cn } from '@/lib/utils';
+
+const lobster = Lobster({ subsets: ['latin'], weight: '400' });
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className='sticky top-0 z-10 bg-neutral-700 dark:bg-neutral-800 text-white dark:text-gray-200 shadow-md'>
+      <nav className='container py-4 flex flex-col items-center sm:flex-row justify-between space-y-4 sm:space-y-0'>
+        <h1
+          className={cn(
+            'text-2xl sm:text-3xl text-center whitespace-nowrap',
+            lobster.className,
+          )}
+        >
+          <Link href='/'>Photo Gallery</Link>
+        </h1>
+
+        <div className='flex items-center space-x-6'>
+          <Search />
+
+          <ThemeSwitcher />
+        </div>
+      </nav>
+    </header>
+  );
 }
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,34 @@
 'use client';
 
-export default function Search() {
-  return <div>Search</div>;
-}
+import { useState, FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+
+const Search = () => {
+  const [search, setSearch] = useState<string>('');
+
+  const router = useRouter();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (search) router.push(`/search/${search}`);
+    setSearch('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='relative'>
+      <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400'>
+        <MagnifyingGlassIcon className='h-5 w-5' />
+      </span>
+
+      <input
+        type='text'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className='py-2 border border-gray-400 pl-10 pr-4 w-full text-gray-700 dark:text-white rounded-md focus:border-indigo-700 focus:ring-indigo-600 focus:outline-none focus:ring'
+        placeholder='Search...'
+      />
+    </form>
+  );
+};
+export default Search;
