20e8ab6b0a31ab90d69d36e5973b0381c47cd85c
herman <hermanconnor11@gmail.com>
2023-09-28 15:08:01 -0400

    Add base components, zod, and plaiceholder

---
+++ components/Gallery.tsx
@@ -0,0 +1,5 @@
+interface Props {}
+
+export default function Gallery({}: Props) {
+  return <div>Gallery</div>;
+}
---
+++ components/PhotoContainer.tsx
@@ -0,0 +1,4 @@
+interface Props {}
+export default function PhotoContainer({}: Props) {
+  return <div>PhotoContainer</div>;
+}
