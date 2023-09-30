ead5877327c60399389a622e57158ae35bdd52b1
herman <hermanconnor11@gmail.com>
2023-09-29 22:54:46 -0400

    Add search page and favicon

---
+++ app/[search]/[...myParams]/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from 'next';
+import Gallery from '@/components/Gallery';
+
+interface Props {
+  params: {
+    myParams: (string | undefined)[];
+  };
+}
+
+export async function generateMetadata({
+  params: { myParams },
+}: Props): Promise<Metadata> {
+  const topic = myParams?.[0] ?? 'curated';
+  const page = myParams?.[1] ?? '1';
+
+  return {
+    title: `Results for ${topic} - Page ${page}`,
+  };
+}
+
+const SearchPage = ({ params: { myParams } }: Props) => {
+  const topic = myParams?.[0] ?? 'curated';
+  const page = myParams?.[1] ?? '1';
+
+  return <Gallery topic={topic} page={page} />;
+};
+
+export default SearchPage;
